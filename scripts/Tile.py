1d79d56550345307241beb4e3b67cb1bbdd6ef7f
jashu442 <jashujashu539@gmil.com>
2025-06-19 18:56:03 +0530

    new

--- scripts/Tile.py
+++ scripts/Tile.py
@@ -4,7 +4,7 @@ import pygame
 class tile(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
-        self.image = pygame.image.load("../sprites/grass.png")
+        self.image = pygame.image.load("sprites/grass.png")
         self.rect = self.image.get_rect(topleft=pos)
 
     def update(self, x, y):
